7440be7a467ac8f925a96ddaaea350064cd1fa55
Piyush Prajapat <piyushprajapati@eoxysitsolutionllp.com>
2023-06-14 12:42:32 +0530

    Piyush - 14-06-2023

--- lib/screens/OB_screen.dart
+++ lib/screens/OB_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 class OBScreen extends StatefulWidget {
   const OBScreen({Key? key}) : super(key: key);
 
@@ -9,6 +10,17 @@ class OBScreen extends StatefulWidget {
 class _OBScreenState extends State<OBScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery
+        .of(context)
+        .size;
+    return  Scaffold(
+      body: Column(
+        children: [
+          SvgPicture.asset((
+          'assets/images/ziplogo.svg'
+      ),)
+        ],
+      ),
+    );
   }
 }
