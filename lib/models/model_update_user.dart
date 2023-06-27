1236329bad09caf15109392b9260209fb470065e
shivwp <shivkumawat1985@hotmail.com>
2023-06-27 15:21:29 +0530

    piyush 27-06-2023

---
+++ lib/models/model_update_user.dart
@@ -0,0 +1,19 @@
+
+class ModelUpdateUser {
+  bool? status;
+  String? message;
+
+  ModelUpdateUser({this.status, this.message});
+
+  ModelUpdateUser.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['message'] = this.message;
+    return data;
+  }
+}
