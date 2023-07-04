389a9ee0ecdba7dc77311b9cef8d66138d64e4c8
shivwp <shivkumawat1985@hotmail.com>
2023-07-04 10:33:12 +0530

    piyush 27-06-2023

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -13,8 +14,11 @@ import 'package:zip/widgets/common_colour.dart';
 
 import '../controller/number_controller.dart';
 import '../controller/update_user.dart';
+import '../models/model_setting.dart';
 import '../models/model_verify_africa.dart';
 import '../models/verify_africa.dart';
+import '../repository/setting_repo.dart';
+import '../repository/verify_africa_b.dart';
 import '../resourses/api_constant.dart';
 
 class OtpScreen extends StatefulWidget {
@@ -30,19 +34,82 @@ class _OtpScreenState extends State<OtpScreen> {
   final registorController = Get.put(registerController());
   File image = File("");
 
+  Rx<ModelSetting> setting = ModelSetting().obs;
+  Rx<RxStatus> statusOfSetting = RxStatus.empty().obs;
+
+  Future getSetting() async {
+    await settingRepo().then((value) {
+      setting.value = value;
+      if (value.status == true) {
+        statusOfSetting.value = RxStatus.success();
+      } else {
+        statusOfSetting.value = RxStatus.error();
+      }
+    }
+      // showToast(value.message.toString());
+    );
+  }
+  Rx<RxStatus> statusOfSucess= RxStatus.empty().obs;
+  Future verify() async {
+    await verifyRepo(
+      selfie: base64Image,
+      // phone_email:  numbercontroller.isNumber ? numbercontroller.number:numbercontroller.email,
+      lastName: registorController.lastNameController.text.trim(),
+      firstName: registorController.firstNameController.text.trim(),
+      searchParameter: numbercontroller.isNumberBvn
+          ? numbercontroller.numberBvn
+          : numbercontroller.emailBvn,
+      dob: registorController.dateOfBirthController.text.trim(),
+      apiKey: setting.value.data!.authKey.toString(),
+      userid: setting.value.data!.userID.toString(),
+    ).then((value) {
+      statusOfSucess.value = RxStatus.success();
+      showToast(value.response!.status);
+      showToast(value.verificationStatus);
+      modelVerifyAfrica.value = value;
+      showToast(value.verificationStatus);
+    }
+      // showToast(value.message.toString());
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getSetting();
+  }
+
+  String base64Image = "";
+
   final numbercontroller = Get.put(numberController());
   Future pickImage() async {
     try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      final image = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+      );
+
       if (image == null) return;
+      if (image.path.toString().contains(".png")) {
+        showToast("dsfdsf");
+        return;
+      }
+
+      print(base64Image);
       final imageTemporary = File(image.path);
       setState(() {
+        List<int> imageBytes = imageTemporary.readAsBytesSync();
+        print(imageBytes);
+        base64Image = base64Encode(imageBytes);
+        // "data:image/jpg;base64,"
+        base64Image = "data:image/${image.path.split(".").last};base64,$base64Image";
         this.image = imageTemporary;
       });
     } on PlatformException catch (e) {
       print('Field to pick img : $e');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     final defaultPinTheme = PinTheme(
@@ -73,121 +140,94 @@ class _OtpScreenState extends State<OtpScreen> {
         ),
         body: SingleChildScrollView(
             child: Form(
-          key: formKeypin,
-          child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0, right: 10),
-                      child: Text(
-                        "Create your unique 4-digits pin!",
-                        style: GoogleFonts.poppins(
-                            color: const Color(0xFF1D1D1D),
-                            fontSize: 22,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: AppTheme.primaryColor)),
-                      child: Center(
-                        child: InkWell(
-                          onTap: () => pickImage(),
-                          // child: image.path != ""
-                          //     ?
-                          //
-                          child:     ClipOval(
-                            child: Image.file(
-                              image,
-                              width: 120,
-                              height: 120,
-                              fit: BoxFit.cover,
-                              errorBuilder: (_,__,___)=> Icon(Icons.person),
+              key: formKeypin,
+              child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0, right: 10),
+                          child: Text(
+                            "Create your unique 4-digits pin!",
+                            style: GoogleFonts.poppins(
+                                color: const Color(0xFF1D1D1D),
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(color: AppTheme.primaryColor)),
+                          child: Center(
+                            child: InkWell(
+                                onTap: () => pickImage(),
+                                // child: image.path != ""
+                                //     ?
+                                //
+                                child: ClipOval(
+                                  child: Image.file(
+                                    image,
+                                    width: 120,
+                                    height: 120,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (_, __, ___) =>
+                                        Icon(Icons.person),
+                                  ),
+                                )),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0, right: 10),
+                          child: Text(
+                            "Please remember this pin. It’ll be used to keep your account secure ",
+                            style: GoogleFonts.poppins(
+                                color: const Color(0xFF1D1D1D),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25.0, right: 25),
+                          child: Center(
+                            child: Pinput(
+                              validator: MultiValidator([
+                                RequiredValidator(errorText: 'Ente r 4 Digit Pin')
+                              ]),
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              controller: registorController.otpcontroller,
+                              keyboardType: TextInputType.number,
+                              length: 4,
+                              defaultPinTheme: defaultPinTheme,
                             ),
-                          )
-
+                          ),
                         ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0, right: 10),
-                      child: Text(
-                        "Please remember this pin. It’ll be used to keep your account secure ",
-                        style: GoogleFonts.poppins(
-                            color: const Color(0xFF1D1D1D),
-                            fontSize: 15,
-                            fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25.0, right: 25),
-                      child: Center(
-                        child: Pinput(
-                          validator: MultiValidator([
-                            RequiredValidator(errorText: 'Ente r 4 Digit Pin')
-                          ]),
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          controller: registorController.otpcontroller,
-                          keyboardType: TextInputType.number,
-                          length: 4,
-                          defaultPinTheme: defaultPinTheme,
+                        SizedBox(
+                          height: size.height * .6,
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: size.height * .6,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        if (formKeypin.currentState!.validate()) {
-                          // registorController.updateUser();
-
-
-                            Map map = <String, String>{};
-                            //
-                           map['phone_email'] = "ayoodapo@yahoo.com";
-
-                           // numbercontroller.isNumber ? numbercontroller.number:numbercontroller.email;
-
-                          verifyAfricaRepo(
-                              fieldName1: 'selfie',
-                              mapData: map,
-                              context: context,
-                              file1: image,
-                            ).then((value) {
-                              if (value.status == true) {
-                                // doctorprofileController.getDataProfile();
-                              }
-                              showToast(value.message.toString());
-                            });
-
-                            // Get.toNamed(MyRouters.doctorNavbar);
-
-                          }
-                        },
-
-                      child: CustomOutlineBoder(
-                        title: "Next",
-                        backgroundColor: Colors.white,
-                        textColor: AppTheme.buttonColor,
-                        onPressed: () {
-                          Get.toNamed(MyRouters.otpScreen);
-                        },
-                      ),
-                    )
-                  ])),
-        )));
+                        InkWell(
+                          onTap: () {
+                            verify();
+                          },
+                          child: CustomOutlineBoder(
+                            title: "Next",
+                            backgroundColor: Colors.white,
+                            textColor: AppTheme.buttonColor,
+                            onPressed: () {
+                              Get.toNamed(MyRouters.otpScreen);
+                            },
+                          ),
+                        )
+                      ])),
+            )));
   }
 }
